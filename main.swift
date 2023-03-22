7586c43787aa7576a311726e34e6ebb0ee12c983
Marcello Gonzatto Birkan <82703050+MelloBirkan@users.noreply.github.com>
2023-03-21 22:57:54 -0300

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,24 +1,76 @@
-let name: String = "Marcello"
-let number: Int = 7
-let decimal: Double = 24.7
-var albums: [String] = ["Red", "Fearless"]
-var user: [String: String] = ["id": "@melloBirkan"]
-var books: Set<String> = Set([
-  "The BLuest Eye",
-  "Foundation",
-  "Girl, Woman, Other"
-])
-var soda: [String] = ["Coke", "Pepsi"]
-var team: [String] = [String]()
-var cities: [String] = []
-var clues = [String]()
+let footballPlayers = ["Cr7", "Messi"]
+var numbers = [1, 2, 4, 5]
+let temperatures = [23.6, 35.1, 21.5]
 
-enum UiStyle {
-  case light, dark, system
+print(numbers[0])
+print(footballPlayers[1])
+
+numbers.append(9)
+numbers.append(12)
+numbers.append(21)
+
+var scores = Array<Int>()
+scores.append(100)
+scores.append(1000)
+scores.append(10000)
+print(scores[2])
+
+var albums = [String]()
+albums.append("Folklore")
+albums.append("Red")
+
+var games = ["ff7r"]
+ print(albums.count)
+games.remove(at: 0)
+albums.removeAll()
+games.contains("gta")
+
+let cities = ["London", "Tokyo"]
+cities.sorted()
+cities.reversed()
+
+// Dictionaries
+
+var employee = ["name": "Taylor Swift", "job": "Singer", "Location": "Nashville"]
+
+print(employee["name", default: "Unknown"])
+print(employee["job", default: "Unknown"])
+
+let hasGraduated = [
+  "Eric": false,
+  "Maeve": true,
+  "Otis": false
+]
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+
+// Sets
+
+var actors = Set(["Denzel", "Tom", "Brad"])
+actors.insert("Nicolas")
+print(actors)
+
+var sets = Set<String>()
+// In comparison, calling contains() on a set runs so fast you’d struggle to measure it meaningfully.
+
+// Tip: Alongside contains(), you’ll also find count to read the number of items in a set, and sorted() to return a sorted array containing the the set’s items.
+
+// ENUM
+
+enum Weekday {
+  case monday
+  case tuesday
+  case wednesday
+  case thursday
+  case friday  
 }
 
-var style = UiStyle.light
-style = .dark
+var day = Weekday.monday
+day = Weekday.tuesday
+day = .thursday
+print(day)
 
-let username: String
-username = "@twostraws"
+enum Weekdays {
+    case monday, tuesday, wednesday, thursday, friday
+}
